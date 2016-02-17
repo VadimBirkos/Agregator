401acbae140a3a77292b0b7f0bbe9e337547f866
VadimBirkos <vadim.birkos@gmail.com>
2016-02-18 00:00:30 +0200

    release json menu item's parse and remove "hardcode" dictionary

--- MvcPl/Controllers/ClientController.cs
+++ MvcPl/Controllers/ClientController.cs
@@ -1,87 +1,83 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Caching;
 using System.Web.Mvc;
 using Bll.Implementation.Clients;
 using CommonInterface;
 using Dal.Interface;
+using Newtonsoft.Json.Linq;
 using PagedList;
 
 namespace Agregator.Controllers
 {
     public class ClientController : Controller
     {
+        private const string Path = @"D:\Кодинг\Git\Agregator\MvcPl\configDate.json";
+        private Dictionary<string, List<MenuItem>> menu;
         private ObjectCache _cache;
         private IEnumerable<PartyModel> _tempList;
-        private readonly RelaxClient _relaxClient;
+
+        private RelaxClient RelaxClient { get; }
 
         public ClientController()
         {
             _cache = MemoryCache.Default;
-            _relaxClient = new RelaxClient();
+            RelaxClient = new RelaxClient();
         }
 
         public ClientController(RelaxClient relaxClient)
         {
-            _relaxClient = relaxClient;
+            RelaxClient = relaxClient;
         }
 
         // GET: Client
 
-        #region
+        #region menuContent
 
-        private readonly Dictionary<string, List<MenuItem>> _menu = new Dictionary<string, List<MenuItem>>()
+        private void ParseJSONMenuItems()
         {
+            if (_cache["menu"] != null)
             {
-                "Развлечения", new List<MenuItem>()
-                {
-                    new MenuItem("Ночные клубы", @"http://www.relax.by/cat/ent/clubs/"),
-                    new MenuItem("Караоке-клубы", @"http://www.relax.by/cat/ent/karaoke/"),
-                    new MenuItem("Боулинг", @"http://www.relax.by/cat/ent/bowlings/"),
-                    new MenuItem("Бильярд", @"http://www.relax.by/cat/ent/billiards/"),
-                    new MenuItem("Бани и сауны", @"http://www.relax.by/cat/ent/clubs/"),
-                    new MenuItem("Коттеджи и усадьбы", @"http://www.relax.by/cat/tourism/cottages/"),
-                    new MenuItem("Интернет кафе", @"http://www.relax.by/cat/ent/internet-cafe/"),
-                    new MenuItem("Кинотеатры", @"http://www.relax.by/cat/ent/kino/"),
-                    new MenuItem("Театры", @"http://www.relax.by/cat/ent/theatres/"),
-                    new MenuItem("Музеи и галереи", @"http://www.relax.by/cat/ent/museums/"),
-                    new MenuItem("Концертные залы", @"http://www.relax.by/cat/ent/halls/"),
-                    new MenuItem("Квесты", @"http://www.relax.by/cat/active/quest/")
-                }
-            },
-            {
-                "Все для праздника", new List<MenuItem>()
-                {
-                    new MenuItem("Свадебные салоны", @"http://www.relax.by/cat/holiday/wedding/")
+                var tempMenu = _cache["menu"];
+                menu = (Dictionary<string, List<MenuItem>>)tempMenu;
+                return;
+            }
 
-                }
-            },
+            menu = new Dictionary<string, List<MenuItem>>();
+            var token = JToken.Parse(System.IO.File.ReadAllText(Path));
+            foreach (var upper in token)
             {
-                "Здоровье и красота", new List<MenuItem>()
+                foreach (var menuHeader in upper)
                 {
-                    new MenuItem("Салон красоты", @"http://www.relax.by/cat/health/beauty/")
+                    foreach (var menuItem in menuHeader["Content"])
+                    {
+                        if (menu.ContainsKey(menuHeader["Name"].ToString()))
+                            menu[menuHeader["Name"].ToString()].Add(
+                                new MenuItem(menuItem["Name"].ToString(), menuItem["Link"].ToString()));
+                        else
+                            menu.Add(menuHeader["Name"].ToString(), new List<MenuItem>()
+                            {
+                                new MenuItem(menuItem["Name"].ToString(), menuItem["Link"].ToString())
+                            });
+                    }
                 }
             }
-        };
-
-        public RelaxClient RelaxClient
-        {
-            get
-            {
-                return _relaxClient;
-            }
+            _cache["menu"] = menu;
         }
 
         #endregion
 
         public ActionResult Index()
         {
-            return View(_menu);
+            if(menu==null) ParseJSONMenuItems();
+            return View(menu);
         }
 
         public ActionResult FilterCatalog(string  key)
         {
-            var catalog = _menu.Where(item => item.Key == key).ToList()[0].Value;
+            if (menu == null) ParseJSONMenuItems();
+            var catalog = menu?.Where(item => item.Key == key).ToList()[0].Value;
             return PartialView(catalog);
         }
 
@@ -98,11 +94,6 @@ namespace Agregator.Controllers
             var a = _cache["last"];
             _tempList = (List<PartyModel>)a;
             return PartialView(_tempList.ToPagedList(page,20));
-        }
-
-        public ActionResult TestPage()
-        {
-            return View();
         }
     }
 }

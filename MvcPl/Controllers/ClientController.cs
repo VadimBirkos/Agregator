0e28d1b2c0d9bc00b39ee4ab1272c90b805ff3be
VadimBirkos <vadim.birkos@gmail.com>
2016-02-18 11:45:12 +0200

    refactoring json parser

--- MvcPl/Controllers/ClientController.cs
+++ MvcPl/Controllers/ClientController.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Caching;
 using System.Web.Mvc;
+using Agregator.Infrastructure;
 using Bll.Implementation.Clients;
-using CommonInterface;
 using Dal.Interface;
-using Newtonsoft.Json.Linq;
 using PagedList;
 
 namespace Agregator.Controllers
@@ -14,86 +12,51 @@ namespace Agregator.Controllers
     public class ClientController : Controller
     {
         private const string Path = @"D:\Кодинг\Git\Agregator\MvcPl\configDate.json";
-        private Dictionary<string, List<MenuItem>> menu;
-        private ObjectCache _cache;
+        private readonly ObjectCache _cache;
+        private readonly JsonParser _jsonParser;
         private IEnumerable<PartyModel> _tempList;
 
         private RelaxClient RelaxClient { get; }
 
         public ClientController()
         {
+            _jsonParser = new JsonParser();
             _cache = MemoryCache.Default;
             RelaxClient = new RelaxClient();
         }
 
-        public ClientController(RelaxClient relaxClient)
+        public ClientController(RelaxClient relaxClient, JsonParser jsonParser)
         {
             RelaxClient = relaxClient;
+            this._jsonParser = jsonParser;
         }
-
-        // GET: Client
-
-        #region menuContent
-
-        private void ParseJSONMenuItems()
-        {
-            if (_cache["menu"] != null)
-            {
-                var tempMenu = _cache["menu"];
-                menu = (Dictionary<string, List<MenuItem>>)tempMenu;
-                return;
-            }
-
-            menu = new Dictionary<string, List<MenuItem>>();
-            var token = JToken.Parse(System.IO.File.ReadAllText(Path));
-            foreach (var upper in token)
-            {
-                foreach (var menuHeader in upper)
-                {
-                    foreach (var menuItem in menuHeader["Content"])
-                    {
-                        if (menu.ContainsKey(menuHeader["Name"].ToString()))
-                            menu[menuHeader["Name"].ToString()].Add(
-                                new MenuItem(menuItem["Name"].ToString(), menuItem["Link"].ToString()));
-                        else
-                            menu.Add(menuHeader["Name"].ToString(), new List<MenuItem>()
-                            {
-                                new MenuItem(menuItem["Name"].ToString(), menuItem["Link"].ToString())
-                            });
-                    }
-                }
-            }
-            _cache["menu"] = menu;
-        }
-
-        #endregion
 
         public ActionResult Index()
         {
-            if(menu==null) ParseJSONMenuItems();
+            var menu = _jsonParser.ParseJSONMenuItems(Path);
             return View(menu);
         }
 
-        public ActionResult FilterCatalog(string  key)
+        public ActionResult FilterCatalog(string key)
         {
-            if (menu == null) ParseJSONMenuItems();
+            var menu = _jsonParser.ParseJSONMenuItems(Path);
             var catalog = menu?.Where(item => item.Key == key).ToList()[0].Value;
             return PartialView(catalog);
         }
 
         private void GetParseList(string url)
         {
-            if(_cache[url] == null)
-            _cache[url] = RelaxClient.GetParties(url);
+            if (_cache[url] == null)
+                _cache[url] = RelaxClient.GetParties(url);
             _cache["last"] = _cache[url];
         }
 
         public ActionResult Parse(int page, string url = null)
         {
-            if(url != null) GetParseList(url);
+            if (url != null) GetParseList(url);
             var a = _cache["last"];
-            _tempList = (List<PartyModel>)a;
-            return PartialView(_tempList.ToPagedList(page,20));
+            _tempList = (List<PartyModel>) a;
+            return PartialView(_tempList.ToPagedList(page, 20));
         }
     }
 }
---
+++ MvcPl/Infrastructure/JsonParser.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Caching;
+using CommonInterface;
+using Newtonsoft.Json.Linq;
+
+namespace Agregator.Infrastructure
+{
+    public class JsonParser
+    {
+        private ObjectCache _cache;
+
+
+        public JsonParser()
+        {
+            _cache = MemoryCache.Default;
+            
+        }
+
+        public Dictionary<string, List<MenuItem>> ParseJSONMenuItems(string path)
+        {
+            var menu = new Dictionary<string, List<MenuItem>>();
+            if (_cache["menu"] != null)
+            {
+                var tempMenu = _cache["menu"];
+                menu = (Dictionary<string, List<MenuItem>>) tempMenu;
+                return menu;
+            }
+            var token = JToken.Parse(File.ReadAllText(path));
+            foreach (var upper in token)
+            {
+                foreach (var menuHeader in upper)
+                {
+                    foreach (var menuItem in menuHeader["Content"])
+                    {
+                        if (menu.ContainsKey(menuHeader["Name"].ToString()))
+                            menu[menuHeader["Name"].ToString()].Add(
+                                new MenuItem(menuItem["Name"].ToString(), menuItem["Link"].ToString()));
+                        else
+                            menu.Add(menuHeader["Name"].ToString(), new List<MenuItem>()
+                            {
+                                new MenuItem(menuItem["Name"].ToString(), menuItem["Link"].ToString())
+                            });
+                    }
+                }
+            }
+            _cache["menu"] = menu;
+            return menu;
+        }
+
+
+    }
+}
